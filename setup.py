831ec3ab5c0d69c068c19e24a62c4bf33441d939
Arian Jamasb <arian.jamasb@roche.com>
2023-06-13 16:52:26 +0200

    linting

--- setup.py
+++ setup.py
@@ -2,18 +2,6 @@ import numpy
 from Cython.Build import cythonize
 from setuptools import Extension, setup
 
-#setup(
-#    name='cpdb',
-#    version='1.0.0',
-#    ext_modules=[
-#        Extension(
-#            'cpdb.parser',
-#            sources=['cpdb/parser.pyx'],
-#            include_dirs=[numpy.get_include()]
-#        ),
-#    ],
-#)
-
 setup(
     name='cpdb',
     ext_modules=cythonize(["cpdb/parser.pyx"], compiler_directives={'language_level' : "3"}),
--- test.py
+++ test.py
@@ -2,13 +2,11 @@ import os
 import time
 
 import numpy
-import pandas as pd
 import pyximport
 
 pyximport.install(setup_args={
                               "include_dirs": numpy.get_include()},
                   reload_support=True)
-import numpy as np
 from cpdb import parse_pdb
 
 DATA_DIR = "./tests/test_data"
@@ -18,10 +16,6 @@ for f in TEST_FILES:
     print(f)
     start = time.time()
     df = parse_pdb(f"{DATA_DIR}/{f}")
-    #df = pd.DataFrame.from_dict(df)
     print(df)
     end = time.time()
-    #print(df)
     print(end - start)
-#print(df)
-#print(pd.DataFrame.from_dict(df))
