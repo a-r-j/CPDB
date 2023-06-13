c0771b7e0df58a8dbf3772987d45c58e997fd83b
Arian Jamasb <arian.jamasb@roche.com>
2023-06-13 17:12:04 +0200

    update setup

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ from setuptools import Extension, setup
 
 setup(
     name='cpdb',
+    version="0.0.1",
+    license="MIT",
+    author="Arian Jamasb",
+    author_email="arian@jamasb.io",
+    url="https://github.com/a-r-j/cpdb",
     ext_modules=cythonize(["cpdb/parser.pyx"], compiler_directives={'language_level' : "3"}),
     include_dirs=[numpy.get_include()],
+    install_requires=["numpy", "pandas"],
 )
