ba74c73fa939b4b4de5802817328bded0d62f7a4
Arian Jamasb <arian.jamasb@roche.com>
2023-06-13 22:31:17 +0200

    fix install

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 import numpy
 from Cython.Build import cythonize
-from setuptools import Extension, setup
+from setuptools import Extension, setup, find_packages
 
 setup(
-    name='cpdb',
-    version="0.0.1",
+    name='cpdb-protein',
+    version="0.1.0",
     license="MIT",
     author="Arian Jamasb",
     author_email="arian@jamasb.io",
     url="https://github.com/a-r-j/cpdb",
-    ext_modules=cythonize(["cpdb/parser.pyx"], compiler_directives={'language_level' : "3"}),
+    ext_modules=cythonize(
+        ["cpdb/parser.pyx"],
+        compiler_directives={'language_level': "3"}
+        ),
     include_dirs=[numpy.get_include()],
     install_requires=["numpy", "pandas"],
+    packages=find_packages()
 )
