c98fbf46f1d0cfba793be247023186448c608050
Arian Jamasb <arian.jamasb@roche.com>
2023-06-26 18:09:36 +0200

    fix test import

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import pyximport
 pyximport.install(setup_args={
                               "include_dirs": numpy.get_include()},
                   reload_support=True)
-from cpdb import parse_pdb
+from cpdb import parse
 
 DATA_DIR = "./tests/test_data"
 TEST_FILES = os.listdir(DATA_DIR)
@@ -15,7 +15,7 @@ TEST_FILES = os.listdir(DATA_DIR)
 for f in TEST_FILES:
     print(f)
     start = time.time()
-    df = parse_pdb(f"{DATA_DIR}/{f}")
+    df = parse(f"{DATA_DIR}/{f}")
     print(df)
     end = time.time()
     print(end - start)
